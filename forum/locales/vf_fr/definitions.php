03bfaed28706f018e5310dec2934fedd567b2770
Arnaud Leymet <arnaud.leymet@gmail.com>
2015-07-19 23:59:15 +0200

    Vanilla plugin - French locale - 2015.07.01p1901 http://vanillaforums.org/addon/vf_fr-locale

---
+++ forum/locales/vf_fr/definitions.php
@@ -0,0 +1,10 @@
+<?php
+
+ $LocaleInfo['vf_fr'] = array (
+  'Locale' => 'fr',
+  'Name' => 'Français / French',
+  'Description' => 'Official French language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/fr/">here</a>.',
+  'Version' => '2015.07.01p1901',
+  'Author' => 'Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/fr/',
+);
