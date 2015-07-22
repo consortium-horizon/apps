837e3fe5c501035211698d8896a2a07d53015b2c
Poko <poko@MacBook-Pro-de-Poko.local>
2015-07-23 00:10:39 +0200

    Initial xmppprebind plugin commit

---
+++ forum/plugins/xmppprebind/class.xmppprebind.plugin.php
@@ -0,0 +1,34 @@
+<?php if (!defined('APPLICATION')) exit;
+
+$PluginInfo['xmppprebind'] = array(
+    'Name'        => "XMPP-Prebind",
+    'Description' => "XMPP-Prebind allows you to establish and maintain a bosh connection to a xmpp server",
+    'Version'     => '1.0.0',
+    'PluginUrl'   => 'https://github.com/pkonecki/vanilla-xmpp-prebind',
+    'Author'      => "Pierre-Olivier Konecki",
+    'AuthorEmail' => 'pkonecki@gmail.com',
+    'AuthorUrl'   => 'https://github.com/pkonecki',
+    'License'     => 'MIT',
+    'RequiredApplications' => array('Vanilla' => '2.2.x')
+);
+
+/**
+ * XMPP-Prebind Plugin
+ *
+ * @author    Pierre-Olivier Konecki <pkonecki@gmail.com>
+ * @copyright 2014 (c) Pierre-Olivier Konecki
+ * @license   MIT
+ * @package   XMPP-Prebind
+ * @since     1.0.0
+ */
+class XMPPPrebindPlugin extends Gdn_Plugin
+{
+
+    public function Base_AuthSuccess_Handler($Sender, $Args){
+        
+        $Sender->InformMessage(implode($Args));
+
+        $Session = Gdn::Session();
+
+    }
+}
