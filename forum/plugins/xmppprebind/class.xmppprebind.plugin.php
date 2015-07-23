16cc586220ea4e42d00f71543e602184e763c34c
Pierre-Olivier Konecki <pierre-olivier@eugensystems.com>
2015-07-23 15:25:54 +0200

    XmppPrebind: Fix autojoin

--- forum/plugins/xmppprebind/class.xmppprebind.plugin.php
+++ forum/plugins/xmppprebind/class.xmppprebind.plugin.php
@@ -33,7 +33,7 @@ class XMPPPrebindPlugin extends Gdn_Plugin
 
     public function Base_Render_Before($Sender) {
         $Sender->AddJsFile('converse.min.js', 'plugins/xmppprebind');
-        $Sender->AddJsFile('converse.autojoin.js', 'plugins/xmppprebind');
+        //$Sender->AddJsFile('converse.autojoin.js', 'plugins/xmppprebind');
     }
 
     public function AssetModel_StyleCss_Handler($Sender) {
@@ -92,6 +92,41 @@ require(['converse'], function (converse) {
         message_carbons: true,
         hide_offline_users: true,
     });
+    function converseAutoJoinChatPlugin( converse ) {
+        var jids = [ 'bar@chat.consortium-horizon.com' ];
+        converse.on('ready', function() {
+            var _transform = function (jid) {
+                var chatbox = converse.chatboxes.get(jid);
+                if (!chatbox) {
+                    var roster_item = converse.roster.get(jid);
+                    if (roster_item === undefined) {
+                        // Assume MUC
+                        converse.chatboxes.create({
+                            'id': jid,
+                            'jid': jid,
+                            'name': Strophe.unescapeNode(Strophe.getNodeFromJid(jid)),
+                            'nick': Strophe.unescapeNode(Strophe.getNodeFromJid(converse.jid)),
+                            'chatroom': true,
+                            'box_id' : b64_sha1(jid)
+                        });
+                    }
+                    converse.chatboxes.create({
+                        'id': jid,
+                        'jid': jid,
+                        'fullname': _.isEmpty(roster_item.get('fullname'))? jid: roster_item.get('fullname'),
+                        'image_type': roster_item.get('image_type'),
+                        'image': roster_item.get('image'),
+                        'url': roster_item.get('url')
+                    });
+                }
+            };
+            if (typeof jids === "string") {
+                _transform(jids);
+            }
+            _.map(jids, _transform);
+        });
+    };
+    converse.plugins.add('converseAutoJoinChatPlugin', converseAutoJoinChatPlugin);
 });
 </script>";
 
