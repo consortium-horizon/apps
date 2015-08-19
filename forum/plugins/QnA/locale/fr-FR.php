94447c2390faf77f91d32ef55e4afa59c2d93caa
Basile Leyraud Jolly <basile.leyraud@gmail.com>
2015-08-19 17:35:50 +0200

    Déploiement module QnA
    modules pour les questions réponses

---
+++ forum/plugins/QnA/locale/fr-FR.php
@@ -0,0 +1,66 @@
+<?php if (!defined('APPLICATION')) exit();
+
+// Discussion / Question
+$Definition['Activity.QuestionAnswer.FullHeadline'] = '%1$s hat %4$s %8$s beantwortet.';
+$Definition['Activity.AnswerAccepted.FullHeadline'] = '%1$s hat %4$s %8$s angenommen.';
+$Definition['Q&A Accepted'] = 'Résolue ✓';
+
+// Q&A Plugin
+
+$Definition['Did this answer the question?'] = 'Ce commentaire répond il à la question ?';
+
+$Definition['You can either ask a question or start a discussion.'] = 'Vous pouveez démarrer une nouvelle discussion ou poser une question.';
+
+$Definition['You can either ask a question or start a discussion. Choose what you want to do below.'] = 'Vous pouvez démarrer une nouvelle discussion ou poser une question. Faites votre choix ci-dessous.';
+
+$Definition['Ask a Question'] = 'Poser une nouvelle question';
+
+$Definition['Question Title'] = 'Titre de la question';
+
+$Definition['Ask Question'] = 'Poser une question';
+
+$Definition['Question'] = 'Question';
+
+$Definition['Answers'] = 'Réponses';
+
+$Definition['Accept'] = 'Ce commentaire répond à la question';
+
+$Definition['Reject'] = 'Cette réponse est érronée';
+
+$Definition['Yes'] = 'Oui';
+
+$Definition['No'] = 'Non';
+
+$Definition['Don\'t know'] = 'Je ne sais pas';
+
+$Definition['Cancel'] = 'Annuler';
+
+$Definition['Answer'] = 'Réponse';
+
+$Definition['Accepted'] = 'Acceptée';
+
+$Definition['Rejected'] = 'Rejetée';
+
+$Definition['Unanswered'] = 'En attente de réponse';
+
+$Definition['Answered'] = 'Résolue ✓';
+
+$Definition['Dismissable'] = 'Peut être ignorée';
+
+$Definition['QnA $QnA Answer'] = 'QnA $QnA réponse';
+
+$Definition['$QnA Answer'] = 'Réponse $QnA';
+
+$Definition['Reject this answer.'] = 'Rejeter cette réponse';
+
+$Definition['Accept this answer.'] = 'Accepter cette réponse';
+
+$Definition['Click accept or reject beside an answer.'] = '&Uuml;ber einer Antwort <i>Annehmen</i> oder <i>Ablehnen</i> anklicken.';
+
+$Definition['This question\'s answer has been accepted.'] = 'La réponse à cette question a été formulée.';
+
+$Definition['You\'ve asked questions that have now been answered'] = 'vous avez posez une ou des questions qui comportent désormais des réponses !';
+
+$Definition['<a href=\"{/discussions/mine?qna=Answered,url}\">You\'ve asked questions that now have answers</a>. Make sure you accept/reject the answers.'] = '<a href=\"{/discussions/mine?qna=Answered,url}\">Vos questions comportent maintenant des réponses !</a>. Allez vite les consulter !';
+
+$Definition['Someone\'s answered your question. You need to accept/reject the answer.'] = 'Quelqu\'un a répondu à votre question ! Consultez vite les commentaires !';
