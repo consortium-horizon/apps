8238c21466dd6f95ee6faf721540ecad0941d0bb
Basile Leyraud Jolly <basile.leyraud@gmail.com>
2015-09-17 14:01:53 +0200

    Version fr de Yaga featured badges

---
+++ forum/plugins/YagaFeaturedBadges/locale/fr/definitions.php
@@ -0,0 +1,10 @@
+<?php if (!defined('APPLICATION')) exit();
+/* Copyright 2014 Zachary Doll */
+$Definition['Featured Badges'] = 'Badges mis en avant';
+$Definition["You are editing %s's featured badges."] = "Vous éditez les badges mis en avant pour %s.";
+$Definition['FeaturedBadges.Noscript.Instructions'] = 'Enter the Badge IDs you would like to feature below. You can only feature badges you have earned. Click save when finished. Please enable javascript for a nicer interface.';
+$Definition['FeaturedBadges.Normal.Instructions'] = 'Selectionner les 3 badges que vous souhaiteriez mettre en avant en les faisant glisser depuis la zone inférieure vers la zone supérieure. Vous pouvez également réorganiser la liste supérieur en faisant glisser les badges les uns par rapport aux autres. Noubliez pas de sauvegarder une fois terminé !';
+$Definition['No Badge'] = 'Pas de badge';
+$Definition['Badge 1'] = 'Badge 1';
+$Definition['Badge 2'] = 'Badge 2';
+$Definition['Badge 3'] = 'Badge 3';
