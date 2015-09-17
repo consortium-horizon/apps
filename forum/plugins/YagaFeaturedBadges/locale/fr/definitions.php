74ffd08aae1c996d4b26ae41588c4716f83510d4
Basile Leyraud Jolly <basile.leyraud@gmail.com>
2015-09-17 14:07:20 +0200

    corrections de fautes d'ortograffff

--- forum/plugins/YagaFeaturedBadges/locale/fr/definitions.php
+++ forum/plugins/YagaFeaturedBadges/locale/fr/definitions.php
@@ -2,8 +2,8 @@
 /* Copyright 2014 Zachary Doll */
 $Definition['Featured Badges'] = 'Badges mis en avant';
 $Definition["You are editing %s's featured badges."] = "Vous éditez les badges mis en avant pour %s.";
-$Definition['FeaturedBadges.Noscript.Instructions'] = 'Enter the Badge IDs you would like to feature below. You can only feature badges you have earned. Click save when finished. Please enable javascript for a nicer interface.';
-$Definition['FeaturedBadges.Normal.Instructions'] = 'Selectionner les 3 badges que vous souhaiteriez mettre en avant en les faisant glisser depuis la zone inférieure vers la zone supérieure. Vous pouvez également réorganiser la liste supérieur en faisant glisser les badges les uns par rapport aux autres. Noubliez pas de sauvegarder une fois terminé !';
+$Definition['FeaturedBadges.Noscript.Instructions'] = 'Entrez l\'identifiant des badges que vous souhaitez mettre en avant. Vous ne pouvez mettre en avant que les badges que vous avez reçu. Cliquez sur sauvegarder une fois terminé. Autorisez javascript pour une interface plus agréable.';
+$Definition['FeaturedBadges.Normal.Instructions'] = 'Selectionner les 3 badges que vous souhaiteriez mettre en avant en les faisant glisser depuis la zone inférieure vers la zone supérieure. Vous pouvez également réorganiser la liste supérieure en faisant glisser les badges les uns par rapport aux autres. Noubliez pas de sauvegarder une fois terminé !';
 $Definition['No Badge'] = 'Pas de badge';
 $Definition['Badge 1'] = 'Badge 1';
 $Definition['Badge 2'] = 'Badge 2';
