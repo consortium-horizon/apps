496532d5950f0fa7ad80f9eb42a4ce46a93e6521
Basile Leyraud Jolly <basile.leyraud@gmail.com>
2015-09-22 15:25:46 +0200

    M'en fout si fre3k me dit qqchose

--- forum/plugins/postonregister/class.postonregister.plugin.php
+++ forum/plugins/postonregister/class.postonregister.plugin.php
@@ -7,9 +7,64 @@ $PluginInfo['postonregister'] = array(
    'Author' => 'Vladvonvidden',
 );
 
+    $var="Aucun";
+
 class postonregister extends Gdn_Plugin {
 
-  public function entryController_RegisterValidation_handler($sender) {
-    var_dump($sender->Form->_FormValues);
+      public function entryController_RegisterValidation_handler($sender) {
+        $GLOBALS['var'] = $sender->Form->_FormValues['Aqueljeujouezvousgalement'];
+      }
+
+  public function userModel_afterRegister_handler($sender, $Args) {
+    // Get user ID from sender
+    $userID = $sender->EventArguments['UserID'];
+    // Retreive user object
+    $user = $sender->GetID($userID);
+    // Get UserName
+    $name = GetValue('Name', $user, $Default = FALSE, $Remove = FALSE);
+    // Get first visit date
+    $date = Gdn_Format::ToDateTime();
+    // Create new discussionModel
+    $DiscussionModel = new DiscussionModel();
+    // Feed it ! Feeeeeeeeed it !
+    $SQL = Gdn::Database()->SQL();
+    // Where you wanna insert the discussion (which category)
+    $Discussion['CategoryID'] = '9';
+    // Discussion Format (BBcode)
+    $Discussion['Format'] = 'BBCode';
+    // Discussion title
+    $Discussion['Name'] = '[' . (string) $GLOBALS['var'] . ']' . (string) $GLOBALS['var'] . ' [En attente de validation]';
+    // Discussion content
+    $Discussion['Body'] = '[b]Pour quel jeu en particulier postulez-vous dans la Guilde ?[/b]
+
+               '
+               . $GLOBALS['var'] .
+               '<br>
+               [b]A quels autres jeux jouez-vous également ?[/b]
+
+               '
+               . $GLOBALS['var'] .
+               '<br>
+               [b]Comment avez-eu connaissance du Consortium Horizon ?[/b]
+
+               '
+               . $GLOBALS['var'] .
+               '<br>
+               [b]Dites-en un peu plus sur vous :[/b]
+
+               '
+               . $GLOBALS['var'] .
+               '<br>
+               En attente de validation par un modérateur';
+    // Date of creation
+    $Discussion['DateInserted'] = $date;
+    // Date of last comment
+    $Discussion['DateLastComment'] = $date;
+    // The author
+    $Discussion['InsertUserID'] = $userID ;
+    // Insert in the right category
+    $DiscussionID = $SQL->Insert('Discussion', $Discussion);
+    // If everything is ok, refresh discussion count
+    if ($DiscussionID) { $DiscussionModel->UpdateDiscussionCount($Discussion['CategoryID']) ;}
   }
 }
