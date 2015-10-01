031d03c97247701e98d9ffd55e3fb4a688037f7e
Basile Leyraud Jolly <basile.leyraud@gmail.com>
2015-10-01 14:24:28 +0200

    moar pushes

--- forum/plugins/Organigramme/views/organigramme.php
+++ forum/plugins/Organigramme/views/organigramme.php
@@ -4,26 +4,31 @@
 
 <?php
     if ($this->data('admins')) {
-        echo "<h2>Les administrateurs</h2>";
-        echo "<p>Si t'as un problème technique, réfléchi et vide ton cache ... sinon, demande leur de l'aide.</p>";
+        echo "<div class='OrgContainer'>";
+        echo "<div class='OrgContainerPic'><span>Admins</span></div>";
+        echo "<div class='OrgContainerContent'>";
+        echo "<h2 class='Title'>Les administrateurs</h2>";
+        echo "<div class='Description'>Si t'as un problème technique, réfléchi et vide ton cache ... sinon, demande leur de l'aide.</div>";
         echo "<ul>";
         $count = 0;
         $pics = $this->data('adminspics');
         foreach ($this->data('admins') as $key => $value) {
-            echo "<li>";
+            echo "<li class='admins'>";
             echo $pics[$count];
-            echo "<a href='../forum/profile/" . $value->UserID . "/" . $value->Name ."'>" . $value->Name . "</a>";
+            echo "<a class='username' href='../forum/profile/" . $value->UserID . "/" . $value->Name ."'>" . $value->Name . "</a>";
             echo "</li>";
             $count++;
         }
         echo "</ul>";
+        echo "</div>";
+        echo "</div>";
     }
 
     if ($this->data('conseillers')) {
         echo "<div class='OrgContainer'>";
         echo "<div class='OrgContainerPic'><span>Le conseil</span></div>";
         echo "<div class='OrgContainerContent'>";
-        echo "<h2 class='Title'>Les Conseillers</h2>";
+        echo "<h2 class='Title'>Les conseillers</h2>";
         echo "<div class='Description'>Les plus vénérables mammifères du Consortium, ils assurent le bon fonctionnement de la guilde.</div>";
         echo "<ul>";
         $count = 0;
@@ -41,19 +46,24 @@
     }
 
     if ($this->data('modos')) {
-        echo "<h2>Les modérateurs globaux</h2>";
-        echo "<p>Les gardiens du très saint banhammer.</p>";
+        echo "<div class='OrgContainer'>";
+        echo "<div class='OrgContainerPic'><span>Modos</span></div>";
+        echo "<div class='OrgContainerContent'>";
+        echo "<h2 class='Title'>Les modérateurs globaux</h2>";
+        echo "<div class='Description'>Les gardiens du très saint banhammer.</div>";
         echo "<ul>";
         $count = 0;
         $pics = $this->data('modospics');
         foreach ($this->data('modos') as $key => $value) {
-            echo "<li>";
+            echo "<li class='modos'>";
             echo $pics[$count];
-            echo "<a href='../forum/profile/" . $value->UserID . "/" . $value->Name ."'>" . $value->Name . "</a>";
+            echo "<a class='username' href='../forum/profile/" . $value->UserID . "/" . $value->Name ."'>" . $value->Name . "</a>";
             echo "</li>";
             $count++;
         }
         echo "</ul>";
+        echo "</div>";
+        echo "</div>";
     }
 
     if ($this->data('refPS2')) {
