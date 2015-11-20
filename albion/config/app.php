6ae774448cdcccb90afbd0d8a2f6eaabab0d05d6
Pierre-Olivier Konecki <pierre-olivier@eugensystems.com>
2015-11-20 17:54:20 +0100

    Albion: maj conf

--- albion/config/app.php
+++ albion/config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => 'http://www.consortium-horizon.com/albion/',
 
     /*
     |--------------------------------------------------------------------------
