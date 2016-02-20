db3e37c84f9b61aed4a499db4f3d1276be3d00fc
Z-FiRE <Z-FiRE@users.noreply.github.com>
2016-02-20 14:01:09 +0100

    forum nouveau calendrier : changement terme + texte "aucun évènement..."

--- forum/plugins/DiscussionEvent/views/modules/discussionevent.php
+++ forum/plugins/DiscussionEvent/views/modules/discussionevent.php
@@ -2,32 +2,37 @@
 
 // Vanilla 2.1 compatibility:
 if (!function_exists('PanelHeading')) {
-	function PanelHeading($content, $attributes = '') {
-		return Wrap($content, 'h4', $attributes);
-	}
+  function PanelHeading($content, $attributes = '') {
+    return Wrap($content, 'h4', $attributes);
+  }
 }
 
 if (!function_exists('WriteDiscussionEvent')) {
-	function WriteDiscussionEvent($Discussion, $Prefix = null) {
-	?>
-		<span class="Title">
-			<span class="MItem"><?php echo Gdn_Format::Date($Discussion->DiscussionEventDate, '%e %b'); ?></span>
-			<?php echo Anchor(SliceString(Gdn_Format::Text($Discussion->Name, false), 35), DiscussionUrl($Discussion).($Discussion->CountCommentWatch > 0 ? '#Item_'.$Discussion->CountCommentWatch : ''), 'DiscussionLink'); ?>
-		</span>
-	<?php
-	}
+  function WriteDiscussionEvent($Discussion, $Prefix = null) {
+  ?>
+    <span class="Title">
+      <span class="MItem"><?php echo Gdn_Format::Date($Discussion->DiscussionEventDate, '%e %b'); ?></span>
+      <?php echo Anchor(SliceString(Gdn_Format::Text($Discussion->Name, false), 35), DiscussionUrl($Discussion).($Discussion->CountCommentWatch > 0 ? '#Item_'.$Discussion->CountCommentWatch : ''), 'DiscussionLink'); ?>
+    </span>
+  <?php
+  }
 }
 ?>
 
 <div class="Box BoxDiscussionEvents">
-	<?php echo PanelHeading('<i class="fa fa-calendar"></i> '.t('Upcoming Events')); ?>
-	<ul class="PanelInfo PanelDiscussionEvents DataList">
-		<li class="<?php echo CssClass($Discussion); ?>">
-		<?php
-		foreach ($this->Data('DiscussionEvents')->Result() as $Discussion) {
-			WriteDiscussionEvent($Discussion);
-		}
-		?>
-		</li>
-	</ul>
+  <?php echo PanelHeading('<i class="fa fa-calendar"></i> '.t('Upcoming Events')); ?>
+  <ul class="PanelInfo PanelDiscussionEvents DataList">
+    <li class="<?php echo CssClass($Discussion); ?>">
+    <?php
+      $DiscussionArray = $this->Data('DiscussionEvents')->Result();
+      if ($DiscussionArray != null) {
+        foreach ($DiscussionArray as $Discussion) {
+          WriteDiscussionEvent($Discussion);
+        }
+      } else {
+        echo 'Aucun évènement...';
+      }
+    ?>
+    </li>
+  </ul>
 </div>
