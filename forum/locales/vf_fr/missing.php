b093c5030e4e9477e5b5a31ef4e82f90ed2ff650
Z-FiRE <Z-FiRE@users.noreply.github.com>
2016-03-03 18:57:25 +0100

    forum version fr : mise à jour + modif perso discussion => sujet

--- forum/locales/vf_fr/missing.php
+++ forum/locales/vf_fr/missing.php
@@ -1,5 +1,3 @@
 <?php
 
-$Definition['HeadlineFormat.ConversationMessage'] = '{ActivityUserID,User} sent you a <a href="{Url,html}">message</a>';
-
-$Definition['I confirm that I have received consent to join this community.'] = 'Since I\'m under %d years old, I confirm that I have received consent to join this community.';
+$Definition['Routes are used to redirect users.'] = 'Routes are used to redirect users depending on the URL requested.';
