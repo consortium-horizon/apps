a8527af93990444b8902f1a6e908926d76ed0275
Z-FiRE <Z-FiRE@users.noreply.github.com>
2016-03-05 01:26:32 +0100

    forum locale LCH : mise à jour

--- forum/locales/LCH/site_core.php
+++ forum/locales/LCH/site_core.php
@@ -4,3 +4,5 @@ $Definition['Birthday'] = 'Date de naissance (seul votre âge est public)';
 $Definition['Post Discussion'] = 'Publier';
 $Definition['Post Comment'] = 'Publier';
 $Definition['Leave a Comment'] = 'Ajouter un message';
+$Definition['All Categories'] = 'Accueil du forum';
+$Definition['New Discussion'] = 'Nouveau sujet';
