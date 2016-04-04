8d62432f3feb10e248278b9e7a52bb93058bd33b
Pierre-Olivier Konecki <pierre-olivier@eugensystems.com>
2016-04-04 12:22:55 +0200

    Add bootstrap theme

---
+++ forum/themes/bootstrap/gulpfile.js
@@ -0,0 +1,71 @@
+/* laxcomma: true */
+'use strict';
+
+var gulp = require('gulp')
+  , $ = require('gulp-load-plugins')()
+
+gulp.task('styles', function () {
+  var themes = $.filter('themes/*.css');
+
+  return gulp.src([
+    'less/style.less'
+  , 'less/themes/*.less'
+  ], {base: 'less'})
+    .pipe($.plumber())
+    .pipe($.less())
+    .pipe($.autoprefixer())
+    .pipe($.csso())
+    .pipe(themes)
+    .pipe($.rename({
+      dirname: '/'
+    , prefix: 'custom_'
+    }))
+    .pipe(themes.restore())
+    .pipe(gulp.dest('design'))
+    .pipe($.size({showFiles: true}));
+});
+
+gulp.task('scripts', function () {
+  var dependencies = require('wiredep')()
+    , source = $.filter('js/src/**/*.js');
+
+  return gulp.src((dependencies.js || []).concat([
+    'js/src/main.js',
+    'bower_components/bootstrap/js/transition.js',
+    'bower_components/bootstrap/js/collapse.js'
+  ]))
+    .pipe($.plumber())
+    .pipe($.concat('custom.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('js'))
+    .pipe($.size({showFiles: true}));
+});
+
+gulp.task('wiredep', function () {
+  var wiredep = require('wiredep').stream;
+
+  return gulp.src('less/**/*.less')
+    .pipe(wiredep())
+    .pipe(gulp.dest('less'));
+});
+
+gulp.task('default', ['styles', 'scripts']);
+
+gulp.task('watch',  function () {
+  var server = $.livereload();
+
+  gulp.watch([
+    'design/*.css'
+  , 'js/*.js'
+  , 'views/**/*.tpl'
+  ], function (file) {
+    return server.changed(file.path);
+  });
+
+  gulp.watch('less/**/*.less', ['styles']);
+  gulp.watch('js/src/**/*.js', ['scripts']);
+  gulp.watch('bower.json', ['wiredep']);
+});
+
+// Expose Gulp to external tools
+module.exports = gulp;
