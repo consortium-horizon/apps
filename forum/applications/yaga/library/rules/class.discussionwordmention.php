d7c3a7e9dc8e8de71011713f1daa4f8809640d4e
MrBLJ <basile.leyraud@gmail.com>
2016-04-11 18:53:03 +0200

    Yaga rule addition + discussion word mention

---
+++ forum/applications/yaga/library/rules/class.discussionwordmention.php
@@ -0,0 +1,39 @@
+class DiscussionWordMention implements YagaRule{
+public function Award($Sender, $User, $Criteria) {
+$PostInfo = $Sender->EventArguments['FormPostValues'];
+$PBodyText = val('Body',$PostInfo);
+$PUserID =  val('InsertUserID',$PostInfo);
+    if (strpos(strtolower($PBodyText), strtolower($Criteria->WordToMatch)) !== FALSE) {
+     return $PUserID;
+    }
+ return FALSE;
+}
+public function Form($Form) {
+$String = $Form->Label('Yaga.Rules.DiscussionWordMention.Criteria.Head', 'DiscussionWordMention');
+$String .= $Form->Textbox('WordToMatch', array('class' => 'WideInput'));
+return $String;
+}
+public function Validate($Criteria, $Form) {
+$Validation = new Gdn_Validation();
+$Validation->ApplyRules(array(
+array(
+'Name' => 'WordToMatch', 'Validation' => array('Required')
+)
+));
+$Validation->Validate($Criteria);
+$Form->SetValidationResults($Validation->Results());
+}
+public function Hooks() {
+return array('CommentModel_AfterSaveComment', 'DiscussionModel_AfterSaveDiscussion');
+}
+public function Description() {
+$Description = sprintf(T('Yaga.Rules.DiscussionWordMention.Desc'), C('Vanilla.Comment.MaxLength'));
+return Wrap($Description, 'div', array('class' => 'InfoMessage'));
+}
+public function Name() {
+return T('Yaga.Rules.DiscussionWordMention');
+}
+public function Interacts() {
+return FALSE;
+}
+}
---
+++ forum/plugins/YagaRuleAddition/default.php
@@ -0,0 +1,17 @@
+<?php if (!defined('APPLICATION')) exit();
+
+// Define the plugin:
+$PluginInfo['Yaga Rule Addition'] = array(
+   'Name' => 'Yaga Rule Addition',
+   'Description' => 'Add rules to Yaga Application',
+   'Version' => '1.0',
+   'Author' => "Yaga",
+   'MobileFriendly' => TRUE,
+);
+
+class YagaRuleAdditionPlugin extends Gdn_Plugin {
+    public function Yaga_AfterGetRules_Handler($Sender) {
+    $Rule = new DiscussionWordMention()
+    $Sender->EventArguments['Rules'][get_class($Rule)] = $Rule->Name();
+    }
+}
