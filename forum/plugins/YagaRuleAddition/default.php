ce99e498247b94377bb62ba8db1aaf20ca8d188d
MrBLJ <basile.leyraud@gmail.com>
2016-04-11 19:10:50 +0200

    More commits ... always more
    le mec me dit de copy/pasta en oubliant une virgule PUTAIN

--- forum/plugins/YagaRuleAddition/default.php
+++ forum/plugins/YagaRuleAddition/default.php
@@ -5,12 +5,12 @@ $PluginInfo['YagaRuleAddition'] = array(
    'Name' => 'Yaga Rule Addition',
    'Description' => 'Add rules to Yaga Application',
    'Version' => '1.0',
-   'Author' => "Yaga",
+   'Author' => "Yaga"
 );
 
 class YagaRuleAdditionPlugin extends Gdn_Plugin {
     public function Yaga_AfterGetRules_Handler($Sender) {
-    $Rule = new DiscussionWordMention()
-    $Sender->EventArguments['Rules'][get_class($Rule)] = $Rule->Name();
+      $Rule = new DiscussionWordMention();
+      $Sender->EventArguments['Rules'][get_class($Rule)] = $Rule->Name();
     }
 }
