85aba41e90934dfb062d489136b9e24bfca19adc
Pierre-Olivier Konecki <pierre-olivier@eugensystems.com>
2017-01-25 15:03:04 +0100

    Ajout theme auro2

---
+++ forum/themes/auro2/about.php
@@ -0,0 +1,18 @@
+<?php
+
+$ThemeInfo['Auro2'] = array(
+  'Name'        => 'Auro2',
+  'Description' => "Auro2 for Vanilla has been rewritten from scratch on top of Bootstrap 3 in this second installation of the most popular theme for Vanilla Forums.",
+  'Version'     => '2.7.0',
+  'Url'         => 'https://github.com/AnatoliyAV',
+  'Author'      => 'Anatoliy',
+  'AuthorEmail' => 'anatoliy@auroville.org.in',
+  'AuthorUrl'   => 'http://facknet.ru/',
+  'License'     => 'GNU GPL2',
+  'RequiredApplications' => array('Vanilla' => '2.1.x'),
+  'Options' => array(
+    'Styles' => array(
+      'Default'   => '%s_default'
+    )
+  )
+);
