2c5f9defd28b1077e3a0dea1bd02d2d2b34420ef
Z <Z-FiRE@users.noreply.github.com>
2017-03-10 18:11:58 +0100

    candidature : changement texte "merci"

--- forum/themes/LCH - Le consortium Horizon/views/entry/registerthanks.php
+++ forum/themes/LCH - Le consortium Horizon/views/entry/registerthanks.php
@@ -3,6 +3,7 @@
     <h1><?php echo t('Thank You!'); ?></h1>
 
     <div id="Message">
-        Votre candidature sera examinée par un Référent de section ou un Modérateur.<br>Vous serez informé directement sur votre post de candidature, donc pensez à le consulter.
+        <p>Votre candidature sera examinée par un Référent de section ou un Modérateur.<br>Vous serez informé directement sur votre post de candidature, donc pensez à le consulter.</p>
+        <p>En général, si vous ne donnez pas de signe de vie en une semaine, votre candidature est refusée.</p>
     </div>
 </div>
